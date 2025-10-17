e0db2a6cd62b428d08ae4fe408aade09a4ae2b2d
multiment2 <51969777+multiment2@users.noreply.github.com>
2025-10-17 05:09:32 +0000

    Add PyMuPDF and PyPDF lids

--- pdf_viewerTk.py
+++ pdf_viewerTk.py
@@ -5,12 +5,13 @@ Created on Thu Mar 27 08:28:19 2025
 @author: MVSORO21
 """
 from tkinter import *
+'''
 from pypdf import PdfReader
  
 reader = PdfReader("kol_razum.pdf")
 pages = reader.pages[0]
 page = pages.extract_text()
-
+'''
 root = Tk()
 root.title("METANIT.COM")
 root.geometry("1200x640")
